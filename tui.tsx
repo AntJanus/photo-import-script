379eb21e83b7f96d1c763610ff13fcd1b7bb330c
Antonin Januska <antonin@antjanus.com>
2025-07-26 12:08:38 -0600

    tui example

---
+++ tui.tsx
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from 'react';
+import {render, Text} from 'ink';
+
+const Counter = () => {
+	return <Text color="green">It works!</Text>;
+};
+
+render(<Counter />);
